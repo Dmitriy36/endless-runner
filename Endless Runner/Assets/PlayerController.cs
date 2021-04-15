3346d60bdf13f1a109627ff0c068e9f593c2fc7f
Ignatius <dmitriy_xix@hotmail.com>
2021-04-15 13:35:42 -0400

    Finished with #4.

---
+++ Endless Runner/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void StopJump()
+    {            
+        anim.SetBool("isJumping", false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+        }
+    }
+
+}
