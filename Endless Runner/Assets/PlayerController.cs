8d2601318dfa9cb7ea92565ccd4bae3521af2d5d
Ignatius <dmitriy_xix@hotmail.com>
2021-04-15 14:33:59 -0400

    Finished #5.

--- Endless Runner/Assets/PlayerController.cs
+++ Endless Runner/Assets/PlayerController.cs
@@ -16,12 +16,43 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isJumping", false);
     }
 
+    void StopMagic()
+    {
+        anim.SetBool("isMagic", false);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetBool("isJumping", true);
         }
+
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            anim.SetBool("isMagic", true);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up * 90); // rotates about the "up" axis 
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up * -90); // rotates about the "up" axis 
+        }
+
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(-0.1f, 0, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(0.1f, 0, 0);
+        }
+
     }
 
 }
