81ef15097341200ecc43f98b0882e3e6ce246e33
Ignatius <dmitriy_xix@hotmail.com>
2021-04-15 17:40:40 -0400

    Finished #7.

---
+++ Endless Runner/Assets/Scripts/GenerateWorld.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    GameObject dummyTraveler;
+    public GameObject[] platforms;
+    int platformNumber;
+    int turnDirection;
+
+    void Start()
+    {
+        dummyTraveler = new GameObject("dummy");
+        for(int i = 0; i < 50; i++)
+        {
+            platformNumber = Random.Range(0, platforms.Length);
+            GameObject p = Instantiate(platforms[platformNumber], dummyTraveler.transform.position, dummyTraveler.transform.rotation);            
+
+            if(platforms[platformNumber].tag == "stairsUp")
+            {
+                dummyTraveler.transform.Translate(0,5,0);
+            }
+            else if(platforms[platformNumber].tag == "stairsDown")
+            {
+                dummyTraveler.transform.Translate(0, -5, 0);
+                p.transform.Rotate(new Vector3(0, 180, 0));
+                p.transform.position = dummyTraveler.transform.position;
+            }
+            else if(platforms[platformNumber].tag == "platformTSection")
+            {
+                turnDirection = Random.Range(0, 2);
+                if(turnDirection == 0)
+                {
+                    dummyTraveler.transform.Rotate(new Vector3(0, 90, 0));
+                } 
+                else if(turnDirection == 1)
+                {
+                    dummyTraveler.transform.Rotate(new Vector3(0, -90, 0));
+                }
+                
+                dummyTraveler.transform.Translate(Vector3.forward * -10);
+            }
+
+            dummyTraveler.transform.Translate(Vector3.forward * -10);
+        }
+    }
+
+
+}
