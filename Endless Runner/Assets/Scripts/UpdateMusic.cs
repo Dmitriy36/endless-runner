05dfa3dbabc75ec42d50bb5488c32d36c0ed0fd8
Ignatius <dmitriy_xix@hotmail.com>
2021-05-03 13:20:43 -0400

    Finished #35.

---
+++ Endless Runner/Assets/Scripts/UpdateMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMusic : MonoBehaviour
+{
+    List<AudioSource> music = new List<AudioSource>();
+    void Start()
+    {
+        AudioSource[] allAudioSources = GameObject.FindWithTag("GameData").GetComponentsInChildren<AudioSource>();
+        music.Add(allAudioSources[0]);
+    }
+
+    public void UpdateMusicVolume(float value)
+    {
+        foreach(AudioSource m in music)
+        {
+            m.volume = value;
+        }
+    }
+}
