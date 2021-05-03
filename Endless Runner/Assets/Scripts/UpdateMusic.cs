712e1e9b53a084aa774bfb76390c46c92e8f79e9
Ignatius <dmitriy_xix@hotmail.com>
2021-05-03 14:13:54 -0400

    Update UpdateMusic.cs
    Messing with music shit.

--- Endless Runner/Assets/Scripts/UpdateMusic.cs
+++ Endless Runner/Assets/Scripts/UpdateMusic.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpdateMusic : MonoBehaviour
 {
     List<AudioSource> music = new List<AudioSource>();
     void Start()
     {
+
         AudioSource[] allAudioSources = GameObject.FindWithTag("GameData").GetComponentsInChildren<AudioSource>();
         music.Add(allAudioSources[0]);
+
+        Slider musicSlider = this.GetComponent<Slider>();
+
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+            UpdateMusicVolume(musicSlider.value);
+        }
+        else
+        {
+            musicSlider.value = 1f;
+            UpdateMusicVolume(1f);
+        }
     }
 
     public void UpdateMusicVolume(float value)
     {
+        PlayerPrefs.SetFloat("MusicVolume", value);
         foreach(AudioSource m in music)
         {
             m.volume = value;
